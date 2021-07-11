05877eb32078dd70fd22a2601b702262740ac7a5
jlbuild <juliabuildbot@gmail.com>
2021-07-11 15:03:12 +0000

    libclangex_jll build 0.1.0+0

---
+++ src/libclangex_jll.jl
@@ -0,0 +1,9 @@
+# Use baremodule to shave off a few KB from the serialized `.ji` file
+baremodule libclangex_jll
+using Base
+using Base: UUID
+import JLLWrappers
+
+JLLWrappers.@generate_main_file_header("libclangex")
+JLLWrappers.@generate_main_file("libclangex", UUID("82994860-12e5-56b1-9971-c34819be692e"))
+end  # module libclangex_jll
